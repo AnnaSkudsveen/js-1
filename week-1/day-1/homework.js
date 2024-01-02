d176e0aae956412112c3256c4cb48425f3fe3620
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-01-02 14:45:17 +0100

    finished homework tasks

---
+++ week-1/day-1/homework.js
@@ -0,0 +1,39 @@
+// Task 1
+var city = "London";
+
+let age = 25;
+
+const birthYear = 1998;
+
+console.log(city);
+console.log(age);
+console.log(birthYear);
+
+// Task 2
+let greeting = "Hello, World!";
+
+let quantity = 1998;
+
+let isJavaScriptFun = true;
+
+let thisIsUndefined;
+
+let emptyValue = null;
+
+console.log(typeof greeting);
+console.log(typeof quantity);
+console.log(typeof isJavaScriptFun);
+console.log(typeof thisIsUndefined);
+console.log(typeof emptyValue);
+
+// Task 3
+let numString = "25";
+let num = parseInt(numString);
+console.log(num);
+
+let num2 = 50;
+let numString2 = num2.toString();
+console.log(numString2);
+
+console.log(num + num2);
+console.log(numString + numString2);
