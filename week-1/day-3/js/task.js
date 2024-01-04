75086610e0a38b81c77292c3d0ee5e8d6e35aaa0
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-01-04 13:56:23 +0100

    created files for task, and finished the task

---
+++ week-1/day-3/js/task.js
@@ -0,0 +1,24 @@
+let grade = "A";
+
+switch (grade) {
+    case "A":
+        console.log("Outstanding!");
+        break;
+    case "B":
+        console.log("Very Good!");
+        break;
+    case "C":
+        console.log("Good Effort!");
+        break;
+    case "D":
+        console.log("Needs Improvement.");
+        break;
+    case "E":
+        console.log("Not good.");
+        break;
+    case "F":
+        console.log("Failed. Must Retake the Exam.");
+        break;
+    default:
+        console.log("Invalid grade.")
+}
