bf006571e320493b9606e4c3dcc61fa7928c06f5
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-01-15 12:12:53 +0100

    week 2 day 4, and task

---
+++ week-2/day-4/dark-mode/js/script.js
@@ -0,0 +1,43 @@
+// addEventListnener
+// onclick
+
+const body = document.querySelector("body");
+
+const darkModeButton = document.querySelector(".dark-mode-btn");
+let isDarkMode = false;
+
+// console.log(body);
+// console.log(darkModeButton);
+
+function init() {
+  //Will get dark mode staturs
+  // once gotten wil change body background color
+
+  isDarkMode = window.localStorage.getItem("darkMode");
+
+  if (isDarkMode) {
+    body.style.backgroundColor = "#0A0A0A";
+  } else {
+    body.style.backgroundColor = "#FFF";
+  }
+}
+
+init();
+
+console.log("darkmode before:", isDarkMode);
+
+function toggleMode() {
+  isDarkMode = !isDarkMode;
+
+  window.localStorage.setItem("darkMode", isDarkMode);
+
+  if (isDarkMode) {
+    body.style.backgroundColor = "#0A0A0A";
+    // isDarkMode = false;
+  } else {
+    body.style.backgroundColor = "#FFF";
+    // isDarkMode = true;
+  }
+}
+
+darkModeButton.addEventListener("click", toggleMode);
--- week-2/day-4/js/script2.js
+++ week-2/day-4/js/script2.js
@@ -9,11 +9,14 @@ console.log(myCar);
 const myCarParsed = JSON.parse(myCar);
 console.log(myCarParsed);
 
+const pudding1 = window.localStorage.getItem("puddingFlavor");
+
 const shoppingList1 = window.localStorage.getItem("shoppingList");
 const shoppingListParsed = JSON.parse(shoppingList1);
 
 const coffee1 = window.localStorage.getItem("coffee");
 const coffeeParsed = JSON.parse(coffee1);
 
+console.log(pudding1);
 console.log(coffeeParsed);
 console.log(shoppingListParsed);
