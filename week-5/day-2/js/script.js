a7ea3a9d5fe3c8ba171bf06ab189dc832b3fe517
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-02-01 14:11:01 +0100

    week-5 day-3

--- week-5/day-2/js/script.js
+++ week-5/day-2/js/script.js
@@ -1,13 +1,46 @@
 // data
 
+let todoTask = {
+  name: "foo",
+  body: "bar",
+  userId: 1,
+};
+
 let todo = fetch("https://jsonplaceholder.typicode.com/users")
-  .then(function (httpResponse) {
-    //   console.log(httpResponse);
-    //extract the data
-    return httpResponse.json();
+  .then((httpResponse) => {
+    httpResponse.json();
   })
-  .then(function (todoResult) {
+  .then((todoResult) => {
     console.log(todoResult);
   });
 
 console.log("Other stuff");
+//Show it to the users
+
+fetch("https://jsonplaceholder.typicode.com/users", {
+  method: "POST",
+  body: JSON.stringify({
+    name: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+}).then((httpResponse) => console.log(httpResponse));
+
+fetch("https://jsonplaceholder.typicode.com/users/3"),
+  {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+//Go fetch a promise to forward to .then
+//Asynch ---> await going to fetch, then forward to the next line
+
+async function getTodos() {
+  let httpResponse = await fetch("https://jsonplaceholder.typicode.com/users");
+  let data = httpResponse.json();
+  console.log(data);
+}
